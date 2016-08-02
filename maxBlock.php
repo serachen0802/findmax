e6dc620bb7d31971d3f1af680d8e132977b0ab48
sera <j62988g@yahoo.com.tw>
2016-08-02 06:30:55 +0000

    多個相同陣列

--- maxBlock.php
+++ maxBlock.php
@@ -1,36 +1,52 @@
 <?php
 // ChungYoProbies
- $QAQ = array(
-        array(1, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+header('Content-type: text/html; charset=utf-8');
+//-----原陣列
+//  $Z = array(
+//         array(1, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+//         array(1, 1, 0, 1, 1, 0, 0, 0, 0, 0),
+//         array(0, 0, 0, 1, 1, 0, 0, 0, 0, 0),
+//         array(0, 0, 0, 0, 0, 1, 1, 1, 0, 0),
+//         array(1, 1, 1, 1, 1, 0, 0, 0, 0, 0),
+//         array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+//         array(1, 1, 1, 0, 1, 0, 1, 1, 1, 1),
+//         array(1, 0, 0, 0, 1, 0, 1, 1, 1, 1),
+//         array(1, 0, 0, 0, 1, 0, 1, 1, 1, 1),
+//         array(1, 1, 0, 1, 1, 0, 0, 0, 0, 1)
+//     );
+// -----有兩相同區塊的陣列
+    $Z = array(
+        array(1, 1, 1, 1, 0, 0, 0, 0, 0, 0),
+        array(1, 1, 1, 1, 0, 0, 0, 0, 0, 0),
+        array(0, 1, 1, 1, 1, 0, 0, 0, 0, 0),
+        array(0, 0, 1, 0, 0, 1, 1, 1, 0, 0),
         array(1, 1, 0, 1, 1, 0, 0, 0, 0, 0),
-        array(0, 0, 0, 1, 1, 0, 0, 0, 0, 0),
-        array(0, 0, 0, 0, 0, 1, 1, 1, 0, 0),
-        array(1, 1, 1, 1, 1, 0, 0, 0, 0, 0),
         array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
         array(1, 1, 1, 0, 1, 0, 1, 1, 1, 1),
         array(1, 0, 0, 0, 1, 0, 1, 1, 1, 1),
         array(1, 0, 0, 0, 1, 0, 1, 1, 1, 1),
         array(1, 1, 0, 1, 1, 0, 0, 0, 0, 1)
     );
-    
-    $qoo= new block($QAQ);
-    foreach($qoo->temp as $v){
-       foreach($v as $y){
-        echo $y.",";
+    $Q = new block($Z);
+    //原本的陣列
+    foreach($Q -> temp as $v){
+       foreach($v as $value){
+        echo $value." ";
         }
         echo "<br>";
     }
+
     
     echo "<br>";
     echo "<br>";
     
-    echo $qoo->cc();
+    echo "最大的陣列總和:". $Q -> score();
     
     echo "<br>";
     echo "<br>";
- foreach($qoo->temp as $v){
-       foreach($v as $y){
-        echo $y.",";
+ foreach($Q -> temp as $v){
+       foreach($v as $value){
+        echo $value." ";
         }
         echo "<br>";
     }
@@ -39,87 +55,87 @@
 class block{
     var $temp;
     var $temp2;
-   public function __construct($array){
+    public function __construct($array){
         $this->temp = $array;
         $this->temp2 = $array;
-    }
+        }
     
-    function cc(){
-    for($x = 0; $x < 10; $x ++){
-        for($y = 0; $y < 10; $y ++){
+    // -----計算最大分數
+    // -----如果相同則都顯示
+    function score(){
+        $m = 1;
+    for($x = 0; $x < count($this->temp); $x ++){
+        for($y = 0; $y < count($this->temp[$x]); $y ++){
              $score = 0;
-                if( $this->temp[$x][$y] ){
-                    $this->temp[$x][$y] = 0;
+                if( $this -> temp[$x][$y] ){
+                    $this -> temp[$x][$y] = 0;
                     $score ++;
-                    $score += $this->check($x,$y); 
+                    $score += $this -> check($x,$y); 
                     } 
                 if($score > $max){
                     $max = $score;
-                    $sera['zz']=$x;
-                    $sera['oo']=$y;
+                    $s[0]['$x'] = $x;
+                    $s[0]['$y']= $y;
+                }
+                if($score == $max){
+                    $max = $score;
+                    $s[$m]['$x'] = $x;
+                    $s[$m]['$y'] = $y;
                 }
             }
-            
         }
-    //   echo $max;
-    $this->show($sera['zz'],$sera['oo']);
-    return $max;
-        
+    for($t = 0; $t <= $m; $t ++){
+    $this -> show($s[$t]['$x'],$s[$t]['$y']);
     }
-    
-    
+    return $max;
+    }
+    // -----重複檢查上下左右是否為1
+    // -----並將數字歸0
     function check($x,$y){
-        // global temp;
-        if($this->temp[$x+1][$y]){
+        if($this -> temp[$x+1][$y]){
             $score ++;
-            $this->temp[$x+1][$y] = 0;
-            $score +=$this->check($x+1,$y);
+            $this -> temp[$x+1][$y] = 0;
+            $score += $this->check($x+1,$y);
         }
-        if($this->temp[$x-1][$y]){
+        if($this -> temp[$x-1][$y]){
             $score ++;
-            $this->temp[$x-1][$y] = 0;
-            $score +=$this->check($x-1,$y);
+            $this -> temp[$x-1][$y] = 0;
+            $score += $this->check($x-1,$y);
         }
-        if($this->temp[$x][$y+1]){
+        if($this -> temp[$x][$y+1]){
             $score ++;
-            $this->temp[$x][$y+1] = 0;
-            $score +=$this->check($x,$y+1);
+            $this -> temp[$x][$y+1] = 0;
+            $score += $this->check($x,$y+1);
         }
-        if($this->temp[$x][$y-1]){
+        if($this -> temp[$x][$y-1]){
             $score ++;
-            $this->temp[$x][$y-1] = 0;
-            $score +=$this->check($x,$y-1);
+            $this -> temp[$x][$y-1] = 0;
+            $score += $this->check($x,$y-1);
         }
        return $score;
     }
-        
-        
+    // -----劃出陣列將數值改為1
     function show($x,$y){
-        // global temp;
-        // global temp2;
-        if( $this->temp2[$x+1][$y]){
-            $this->temp2[$x+1][$y]=0;
-            $this->temp[$x+1][$y]=1;
-            $this->show($x+1,$y);
+        if( $this -> temp2[$x+1][$y]){
+            $this -> temp2[$x+1][$y] = 0;
+            $this -> temp[$x+1][$y] = 1;
+            $this -> show($x+1,$y);
         }
-        if( $this->temp2[$x-1][$y]){
-            $this->temp2[$x-1][$y]=0;
-            $this->temp[$x-1][$y]=1;
-            $this->show($x+1,$y);
+        if( $this -> temp2[$x-1][$y]){
+            $this -> temp2[$x-1][$y] = 0;
+            $this -> temp[$x-1][$y] = 1;
+            $this -> show($x-1,$y);
         }
-        if( $this->temp2[$x][$y+1]){
-            $this->temp2[$x][$y+1]=0;
-            $this->temp[$x][$y+1]=1;
-            $this->show($x,$y+1);
+        if( $this -> temp2[$x][$y+1]){
+            $this -> temp2[$x][$y+1] = 0;
+            $this -> temp[$x][$y+1] = 1;
+            $this -> show($x,$y+1);
         }
-        if( $this->temp2[$x][$y-1]){
-            $this->temp2[$x][$y-1]=0;
-            $this->temp[$x][$y-1]=1;
-            $this->show($x,$y-1);
+        if( $this -> temp2[$x][$y-1]){
+            $this -> temp2[$x][$y-1] = 0;
+            $this -> temp[$x][$y-1] = 1;
+            $this -> show($x,$y-1);
         }
-        
     }
 }
-    
-    
     ?>
